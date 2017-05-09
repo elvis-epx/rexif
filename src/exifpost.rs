5c49c9c80cc20b042fd713fc837456141108dbfc
Oliver Hamlet <oliver.hamlet@gmail.com>
2017-05-09 18:54:37 +0100

    Simplify other_tag() implementation

--- src/exifpost.rs
+++ src/exifpost.rs
@@ -3,12 +3,7 @@ use super::types::*;
 /// Find a tag of given type
 fn other_tag(tag: ExifTag, entries: &Vec<ExifEntry>) -> Option<&ExifEntry>
 {
-	for entry in entries {
-		if entry.tag == IfdTag::Exif(tag) {
-			return Some(entry);
-		}
-	}
-	None
+	entries.iter().find(|entry| entry.tag == IfdTag::Exif(tag))
 }
 
 /// Does postprocessing in tags that depend on other tags to have a complete interpretation
@@ -125,5 +120,41 @@ pub fn exif_postprocessing(entry: &mut ExifEntry, entries: &Vec<ExifEntry>)
 
 			_ => (),
 		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn other_tag_should_return_an_entry_with_a_matching_tag_if_one_exists() {
+		let vec = vec![
+			ExifEntry {
+				namespace: Namespace::Standard,
+				tag: IfdTag::Exif(ExifTag::ImageDescription),
+				value: TagValue::Ascii(String::new()),
+				value_more_readable: String::new(),
+			},
+			ExifEntry {
+				namespace: Namespace::Standard,
+				tag: IfdTag::Exif(ExifTag::Make),
+				value: TagValue::Ascii(String::new()),
+				value_more_readable: String::new(),
+			},
+		];
+		let other_entry = other_tag(ExifTag::Make, &vec);
+
+		assert!(other_entry.is_some());
+		assert!(other_entry.unwrap().tag.is_known());
+		assert_eq!(ExifTag::Make as u16, other_entry.unwrap().tag.value());
+	}
+
+	#[test]
+	fn other_tag_should_return_none_if_no_matching_tag_exists() {
+		let vec: Vec<ExifEntry> = Vec::new();
+		let other_entry = other_tag(ExifTag::Make, &vec);
+
+		assert!(other_entry.is_none());
 	}
 }
